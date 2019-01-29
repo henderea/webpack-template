19a42a2e12213adb213657bea8213e6d0d7b8a95
Eric Henderson <henderea@gmail.com>
2019-01-29 13:27:17 -0500

    add the code to register the service worker

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ const $ = require('jquery');
 // require('jquery-ui');
 // const _ = require('lodash');
 // const rand = require('lodash/random');
+
+import registerServiceWorker from '@henderea/static-site-builder/registerServiceWorker';
+registerServiceWorker();
